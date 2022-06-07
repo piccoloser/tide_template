0d400b4c2e68236c391e0fa3ce0c444520d77856
Hunter Logan <63938746+piccoloser@users.noreply.github.com>
2022-06-07 18:18:23 -0400

    Initial Commit

---
+++ src/core.rs
@@ -0,0 +1,39 @@
+pub mod constants {
+    pub const APP_TITLE: &str = "Tide App";
+    pub const APP_VERSION: &str = "0.1.0";
+    pub const TEMPLATE_DIR: &str = "templates/**/*";
+}
+
+pub mod structs {
+    use crate::{context, Tera};
+    use serde;
+    use std::collections::HashMap;
+
+    #[derive(Debug, Clone)]
+    pub struct Context {
+        pub details: HashMap<&'static str, &'static str>,
+        pub tera: Tera,
+    }
+
+    impl Context {
+        pub fn new(template_dir: &str) -> Self {
+            let mut tera = match Tera::new(template_dir) {
+                Ok(tera) => tera,
+                Err(e) => panic!("Failed to initialize Tera: {}", e),
+            };
+
+            Self {
+                details: HashMap::new(),
+                tera: Tera::new(template_dir).unwrap(),
+            }
+        }
+
+        pub fn get(&self) -> tera::Context {
+            context! {
+                "details" => self.details,
+            }
+        }
+    }
+}
+
+pub mod functions {}
